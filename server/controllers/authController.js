b05a6beada5d2d1a04adda7fd5482a59f7b3aa8d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-24 23:55:16 +0530

    creating model controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,19 @@
+const User = require("../models/authModel");
+
+
+exports.registerUser = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+exports.login = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+    
+}
