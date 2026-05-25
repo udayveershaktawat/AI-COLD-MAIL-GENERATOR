448cf966833d2e355103f6d6ab07e99d1773ca4c
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-25 23:27:57 +0530

    model and auth controller updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,17 @@ const User = require("../models/authModel");
 
 exports.registerUser = async(req,res)=>{
     try {
+        const {username,email,password} = req.body;
+        if(!username || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        if(password.length>6){
+            return res.status(400).json({
+                message:"password length must be atleast 6 character"
+            })
+        }
         
+
     } catch (error) {
         
     }
---
+++ server/models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    otp:{
+        type:String
+    },
+    otpExpiry:{
+        type:Date
+    }
+
+
+});
+userSchema.pre("save",async function (next) {
+    if(!this.isModified('password')){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+    next()
+    
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword,this.password)
+    
+}
+
+module.exports = mongoose.model("User",userSchema)
