755a691e88c13860cd896645e355d89d4f21a083
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-26 23:47:23 +0530

    login created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,6 +21,11 @@ exports.registerUser = async(req,res)=>{
 
 exports.login = async (req,res) => {
     try {
+        const {email,password} = req.body;
+         if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
         
     } catch (error) {
         
