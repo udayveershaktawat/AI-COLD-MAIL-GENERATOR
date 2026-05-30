06e8f37c53a1fe5cb1c2c36ee37e5aa648915589
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-30 23:51:05 +0530

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,45 +1,36 @@
 const User = require("../models/authModel");
 
-
-exports.registerUser = async(req,res)=>{
-    try {
-        const {username,email,password} = req.body;
-        if(!username || !email || !password){
-            return res.status(400).json({message:"All fields are required"});
-        }
-        if(password.length>6){
-            return res.status(400).json({
-                message:"password length must be atleast 6 character"
-            })
-        }
-        
-
-    } catch (error) {
-         console.log(error)
-        return res.status(500).json({
-            message:"error while login",
-            error:error.message
-
-        })
-        
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
-
-exports.login = async (req,res) => {
-    try {
-        const {email,password} = req.body;
-         if(!email || !password){
-            return res.status(400).json({message:"All fields are required"});
-        }
-
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            message:"error while login",
-            error:error.message
-
-        })
+    if (password.length > 6) {
+      return res.status(400).json({
+        message: "password length must be atleast 6 character",
+      });
     }
-    
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "error while register",
+      error: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "error while login",
+      error: error.message,
+    });
+  }
+};
