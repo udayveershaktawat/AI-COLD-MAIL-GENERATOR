b75d9be9bdee57996bcdfb45ee4f6139469c4309
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-31 23:42:20 +0530

    u

--- server/server.js
+++ server/server.js
@@ -3,22 +3,20 @@ const cors = require("cors");
 const aiRoutes = require("./routes/aiRoutes");
 const authRoutes = require("./routes/authRoutes");
 
-const app = express()
+const app = express();
 require("dotenv").config();
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth",authRoutes);
-app.use("/api/ai",aiRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/ai", aiRoutes);
 
+app.listen(PORT, () => {
+  console.log(`server started at port number ${PORT}`);
+});
 
-app.listen(PORT,()=>{
-    console.log(`server started at port number ${PORT}`)
-})
-
-app.get("/",(req,res)=>{
-    res.send(`<h1> this is home page</h1>`)
+app.get("/", (req, res) => {
+  res.send(`<h1> this is home page</h1>`);
 });
