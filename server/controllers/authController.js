ffbc8e71022f071a93ca6524a9f85183b5980ef5
udayveershaktawat <udayveershaktawat@gmail.com>
2026-06-03 23:44:12 +0530

    user register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -11,6 +11,24 @@ exports.registerUser = async (req, res) => {
         message: "password length must be atleast 6 character",
       });
     }
+     const userAlreadyExists = await User.findOne({email})
+    if(userAlreadyExists){
+      return res.status(400).json({message:"user already exists"})
+    }
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otpExpiry = new Data(Date.now() + 10*60*1000);
+
+
+    const user  =  await User.create({username,email,password,otp,otpExpiry});
+    res.status(201).json({
+      user,
+      message:"user created successfully"
+    })
+
+
+
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
