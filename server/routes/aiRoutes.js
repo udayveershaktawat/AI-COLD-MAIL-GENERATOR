49ef2e17d987f99be6484ece7926f6972039f426
udayveershaktawat <udayveershaktawat@gmail.com>
2026-06-16 23:23:27 +0530

    ai route created

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,5 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { generateEmail, getHistory } = require('../controllers/aiController');
+const { protect } = require('../middleware/authMiddleware');
 
+router.post('/generate-email', protect, generateEmail);
+router.get('/history', protect, getHistory);
 
 module.exports = router;
